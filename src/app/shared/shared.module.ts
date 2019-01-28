10b0341d2d6f73577a3abc6c81fab65a2cc06b1d
Ermakov Sergey <erm.sergey1989@gmail.com>
2019-01-28 09:33:50 +0200

    feat(shared) add ReactiveFormsModule to SharedModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   exports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
