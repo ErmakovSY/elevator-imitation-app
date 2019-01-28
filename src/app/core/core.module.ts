093840965099b62a36c586269e660b54c5f5246e
Ermakov Sergey <erm.sergey1989@gmail.com>
2019-01-28 09:13:53 +0200

    feat(core) remove unused modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 @NgModule({
   exports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
   ]
 })
 export class CoreModule { }
