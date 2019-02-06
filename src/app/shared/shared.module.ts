955b41f43e360b8c6c7aa146ac5e8ed4cf1c25c2
Ermakov Sergey <erm.sergey1989@gmail.com>
2019-02-06 13:43:14 +0200

    feat(typings) add typings for json files

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+
+const MATERIAL_IMPORTS = [
+  MatCardModule,
+  MatInputModule,
+  MatSliderModule,
+];
 
 @NgModule({
   exports: [
     CommonModule,
-    ReactiveFormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    MATERIAL_IMPORTS
   ]
 })
 export class SharedModule { }
--- src/app/start-page/start-page.component.ts
+++ src/app/start-page/start-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
+import * as config from './../../config.json';
+
 @Component({
   selector: 'app-start-page',
   templateUrl: './start-page.component.html',
@@ -9,20 +11,66 @@ import { FormArray, FormControl, FormGroup } from '@angular/forms';
 export class StartPageComponent implements OnInit {
 
   form: FormGroup;
+  subscribers: any = {};
+
+  get elevatorCount(): FormControl {
+    return this.form.controls['elevatorCount'] as FormControl;
+  }
+
+  get elevators(): FormArray {
+    return this.form.controls['elevators'] as FormArray;
+  }
+
+  get floorCount(): FormControl {
+    return this.form.controls['floorCount'] as FormControl;
+  }
+
+  get floors(): FormArray {
+    return this.form.controls['floors'] as FormArray;
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
+
+    this.subscribers.floorCountChange = this.floorCount.valueChanges
+      .subscribe((floorCount: number) => {
+
+      });
+  }
+
+  private initForm() {
     this.form = this.createForm();
+
+    for (let i = 1; i <= config.default.elevator.count; i++) {
+      this.elevators.push(this.createElevatorForm());
+    }
+
+    for (let i = 1; i <= config.default.flour.count; i++) {
+      this.floors.push(this.createFloorForm());
+    }
   }
 
   private createForm(): FormGroup {
     return new FormGroup({
-      elevatorNumber: new FormControl(1),
+      elevatorCount: new FormControl(config.default.elevator.count),
       elevators: new FormArray([]),
 
-      floorNumber: new FormControl(9),
+      floorCount: new FormControl(config.default.flour.count),
       floors: new FormArray([])
+    });
+  }
+
+  private createElevatorForm() {
+    return new FormGroup({
+      capacity: new FormControl(5)
+    });
+  }
+
+  private createFloorForm() {
+    return new FormGroup({
+      residents: new FormControl(20)
     });
   }
 
