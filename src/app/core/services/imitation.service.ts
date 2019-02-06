2dec8c9489ff2de0c1bdb395e7db8a6064232ea5
Ermakov Sergey <erm.sergey1989@gmail.com>
2019-02-06 19:02:32 +0200

    feat(core:services) update imitation service

--- src/app/core/services/imitation.service.ts
+++ src/app/core/services/imitation.service.ts
@@ -1,23 +1,35 @@
 import { Injectable } from '@angular/core';
 
-import { Observable, Subject } from 'rxjs';
-import { map, shareReplay } from 'rxjs/operators';
+import { combineLatest, ConnectableObservable, Subject } from 'rxjs';
+import { map, publishReplay } from 'rxjs/operators';
 
 import { ImitationOptions } from '../interfaces/imitation-options.interface';
 import { Imitation } from '../models/imitation';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImitationService {
 
-  imitationModel$: Observable<any>;
+  imitationModel$: ConnectableObservable<any>;
   initModel$: Subject<ImitationOptions> = new Subject();
+  runModel$: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
   constructor() {
-    this.imitationModel$ = this.initModel$.pipe(
-      map((options) => new Imitation(options)),
-      shareReplay(1)
-    );
+    this.imitationModel$ = combineLatest(
+      this.initModel$,
+      this.runModel$
+    ).pipe(
+      map(([options, run]: [Imitation, boolean]) => {
+        const model = new Imitation(options);
+        if (run) {
+          model.run();
+        }
+        return model;
+      }),
+        publishReplay(1)
+    ) as ConnectableObservable<any>;
+    this.imitationModel$.connect();
   }
 }
