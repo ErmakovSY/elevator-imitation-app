4548535072342a05b87f85eb61b03eea06b9fa0c
Ermakov Sergey <erm.sergey1989@gmail.com>
2019-02-06 15:11:02 +0200

    feat(core:services) add imitation service

---
+++ src/app/core/services/imitation.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { ImitationOptions } from '../interfaces/imitation-options.interface';
+import { Imitation } from '../models/imitation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImitationService {
+
+  imitationModel$: Observable<any>;
+  initModel$: Subject<ImitationOptions> = new Subject();
+
+  constructor() {
+    this.imitationModel$ = this.initModel$.pipe(
+      map((options) => new Imitation(options)),
+    );
+  }
+}
