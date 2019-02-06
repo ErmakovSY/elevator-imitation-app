09558d58b31a92bbdf080a771a016c7f6f9c050d
Ermakov Sergey <erm.sergey1989@gmail.com>
2019-02-06 16:40:18 +0200

    feat(imitation:floor) add residents count to floors

--- src/app/imitation/floor/floor.component.ts
+++ src/app/imitation/floor/floor.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
 import { IFloor } from '../../core/interfaces/floor.interface';
+import { IResident } from '../../core/interfaces/resident.interface';
+import { ImitationService } from '../../core/services/imitation.service';
+import { ResidentStatus } from '../../core/types/resident/resident-status';
 
 @Component({
   selector: 'app-floor',
@@ -9,12 +15,33 @@ import { IFloor } from '../../core/interfaces/floor.interface';
 })
 export class FloorComponent implements OnInit {
 
+  residents$: Observable<IResident[]>;
+  residentsOnResidenceFloor$: Observable<IResident[]>;
+  residentsWaiting$: Observable<IResident[]>;
+
   @Input() floor: IFloor;
 
-  constructor() { }
+  constructor(private imitationService: ImitationService) { }
 
   ngOnInit() {
+    this.residents$ = this.imitationService.imitationModel$.pipe(
+      map((imitationModel) => {
+        const floor = imitationModel.floors.find(f => f.number === this.floor.number);
+        return floor.residents;
+      })
+    );
 
+    this.residentsOnResidenceFloor$ = this.residents$.pipe(
+      map((residents) =>
+        residents.filter(resident => resident.status === ResidentStatus.ON_RESIDENCE_FLOOR)
+      )
+    );
+
+    this.residentsWaiting$ = this.residents$.pipe(
+      map((residents) =>
+        residents.filter(resident => resident.status === ResidentStatus.WAITING)
+      )
+    );
   }
 
 }
